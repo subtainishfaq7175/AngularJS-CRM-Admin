fcc52cdb4f3c22b7f635c8ff79e85eb4f57481b7
Subtain Ishfaq <s.subtain@gmail.com>
2017-02-21 14:29:51 +0500

    removed unnecessary files

--- angular_ app/employee/employeeHome/employeeHome.js
+++
@@ -1,5 +0,0 @@
-angular.module("app")
-    .controller("HomeController", function(data,$scope,$rootScope) {
-    
-    $rootScope.record=data.data.data;
-});
--- angular_ app/pitch/pitchHome/pitchHome.js
+++
@@ -1,5 +0,0 @@
-angular.module("app")
-    .controller("PitchHomeController", function(data,$scope,$rootScope) {
-    debugger;
-    $rootScope.record=data.data.data;
-});
